b4b57c266606baf4dd3350d080f3a4435249c58e
Pascal Böhler <pascal@bembel.dev>
2023-06-26 15:55:14 +0200

    Initial Commit

---
+++ FaehrtSie-iOS/FaehrtSie_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaehrtSie_iOSApp.swift
+//  FaehrtSie-iOS
+//
+//  Created by Pascal Böhler on 26.06.23.
+//
+
+import SwiftUI
+
+@main
+struct FaehrtSie_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
