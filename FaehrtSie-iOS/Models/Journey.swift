e9db80d765a2396b810dd7b692f768e59516a355
Pascal Böhler <pascal@bembel.dev>
2023-07-01 19:02:30 +0200

    feat: Implemented UI and function of JourneyTile + Changed Journey to fit

--- FaehrtSie-iOS/Models/Journey.swift
+++ FaehrtSie-iOS/Models/Journey.swift
@@ -22,14 +22,50 @@ public enum VehicleType: String {
     case Express = "Express"
 }
 
-public struct Journey: Identifiable {
+public class Journey: Identifiable {
     public var id: Int
     public var name: String
-    public var departurTime: Date
+    public var departureTime: Date
     public var arrivalTime: Date
     public var mot: MoT
     public var startStation: String
     public var endStation: String
     public var isDelayed: Bool
     public var delay: Int // delay in minutes!
+    public var departureStation: String
+    public var arrivalStation: String
+    
+    public init(id: Int, name: String, departureTime: Date, departureStation: String, arrivalTime: Date, arrivalStation: String, mot: MoT, startStation: String, endStation: String, isDelayed: Bool, delay: Int) {
+        self.id = id
+        self.name = name
+        self.departureTime = departureTime
+        self.arrivalTime = arrivalTime
+        self.mot = mot
+        self.startStation = startStation
+        self.endStation = endStation
+        self.isDelayed = isDelayed
+        self.delay = delay
+        self.departureStation = departureStation
+        self.arrivalStation = arrivalStation
+    }
+    
+    public func getDepartureDate() -> String {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        
+        let formattedString = dateFormatter.string(from: departureTime)
+        
+        return formattedString
+    }
+    
+    public func getDepartureTime() -> String {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "HH:mm"
+        
+        let formattedString = dateFormatter.string(from: departureTime)
+        
+        return formattedString
+    }
 }
--- FaehrtSie-iOS/Views/JourneyTileView.swift
+++ FaehrtSie-iOS/Views/JourneyTileView.swift
@@ -12,16 +12,64 @@ struct JourneyTileView: View {
     @State var journey: Journey
     
     var body: some View {
+    
         VStack {
-            Text("\(journey.mot.type.rawValue) \(journey.mot.lineNum)")
-            Text(journey.name)
+            HStack {
+                Text("Monday, \(journey.getDepartureDate())")
+                    .font(.system(size: 25))
+                Spacer()
+                Image(systemName: "clock").font(.system(size: 25))
+                    .foregroundColor(!journey.isDelayed ? .green : .primary)
+                Text("\(journey.getDepartureTime())")
+                    .font(.system(size: 25))
+                    .foregroundColor(!journey.isDelayed ? .green : .primary)
+            }
+            .padding(EdgeInsets(top: 10, leading: 15, bottom: -2, trailing: 15))
+            HStack { // the part underneath
+                Image("\(journey.mot.iconName)")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                Spacer()
+                VStack(alignment: .leading, content: {
+                    HStack {
+                        Image(systemName: "mappin.circle.fill")
+                            .font(.system(size: 20))
+                        Text(journey.departureStation)
+                            .font(.system(size: 20))
+                    }
+                    Spacer().frame(height: 10)
+                    HStack {
+                        Image(systemName: "ellipsis")
+                            .scaledToFit()
+                            .frame(width: 25)
+                        Image(systemName: "clock")
+                        Text("20 Minutes")
+                            .font(.system(size: 15))
+                    }
+                    Spacer().frame(height: 10)
+                    HStack {
+                        Image(systemName: "flag.checkered.circle.fill")
+                            .font(.system(size: 20))
+                        Text(journey.arrivalStation)
+                            .font(.system(size: 20))
+                    }
+
+                })
+            }
+            
+            .padding(EdgeInsets(top: 25, leading: 15, bottom: 25, trailing: 15))
+            .background(.gray)
+            .cornerRadius(25.0)
         }
-        
+        .background(Color(UIColor.lightGray))
+        .cornerRadius(25.0)
+        .padding()
     }
 }
 
 struct JourneyTileView_Previews: PreviewProvider {
     static var previews: some View {
-        JourneyTileView(journey: Journey(id: 1, name: "hi", departurTime: Date.now, arrivalTime: Date.now, mot: MoT(lineNum: 73, type: .Ferry, startPoint: "Ernst-August-Schleuse", endPoint: "Landungsbrücken", iconName: "Hello"), startStation: "Test", endStation: "Huhu", isDelayed: false, delay: 0))
+        JourneyTileView(journey: Journey(id: 1, name: "hi", departureTime: Date.now, departureStation: "Ernst-August-Schleuse", arrivalTime: Date.now, arrivalStation: "Landungsbrücke", mot: MoT(lineNum: 73, type: .Ferry, startPoint: "Ernst-August-Schleuse", endPoint: "Landungsbrücken", iconName: "Faehre73Logo"), startStation: "Test", endStation: "Huhu", isDelayed: true, delay: 0))
     }
 }
