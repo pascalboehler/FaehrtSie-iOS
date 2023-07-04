fc2f8cf6a7806bfec4cafdbf721a832c4aa66b23
Pascal Böhler <pascal@bembel.dev>
2023-07-04 13:38:02 +0200

    feat: Moved SearchJourney Tile to own view file to make recycling components easier

---
+++ FaehrtSie-iOS/Views/SearchJourneyTileView.swift
@@ -0,0 +1,61 @@
+//
+//  SearchJourneyTileView.swift
+//  FaehrtSie-iOS
+//
+//  Created by Pascal Böhler on 04.07.23.
+//
+
+import SwiftUI
+
+struct SearchJourneyTileView: View {
+    
+    @State var startStation: String = ""
+    @State var selectedStartDate: Date = Date.now
+    
+    @State var endStation: String = ""
+    @State var selectedEndDate: Date = Date.now
+    
+    @State var isDeparture = false
+    
+    var body: some View {
+        VStack {
+            VStack {
+                TextField("From...", text: $startStation)
+                    .onTapGesture {
+                        startStation = ""
+                    }
+                    .padding(EdgeInsets(top: 20, leading: 15, bottom: 5, trailing: 15))
+                Divider()
+                TextField("To...", text: $endStation)
+                    .onTapGesture {
+                        endStation = ""
+                    }
+                    .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+                Divider()
+                DatePicker("Departure", selection: $selectedStartDate)
+                    .padding(EdgeInsets(top: 5, leading: 15, bottom: 15, trailing: 15))
+            }
+            .cornerRadius(25.0)
+            .overlay(
+                RoundedRectangle(cornerRadius: 14)
+                    .stroke(Color.gray, lineWidth: 2)
+            )
+            .padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 10))
+            
+            
+            NavigationLink (destination: SelectJourneyView()) {
+                Text("Find journey")
+                    .foregroundColor(.black)
+            }
+            .padding(EdgeInsets(top: 15, leading: 30, bottom: 15, trailing: 30))
+            .background(Color(UIColor.lightGray))
+            .cornerRadius(14)
+        }
+    }
+}
+
+struct SearchJourneyTileView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchJourneyTileView()
+    }
+}
