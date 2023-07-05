94846d2c3de4237a01ba2f626efd057ca98f27df
Pascal Böhler <pascal@bembel.dev>
2023-07-05 10:35:13 +0200

    feat: Added Design file

---
+++ FaehrtSie-iOS/Design.swift
@@ -0,0 +1,15 @@
+//
+//  Design.swift
+//  FaehrtSie-iOS
+//
+//  Created by Pascal Böhler on 05.07.23.
+//
+
+import Foundation
+import SwiftUI
+
+public struct Design {
+    static public let stdCorpColor: Color = Color(red: 0.94, green: 0.58, blue: 0.25) // some orange from the website
+    static public let darkBGColor: Color = Color.gray
+    static public let lightBGColor: Color = Color(UIColor.lightGray)
+}
