db7e23006a28b903afd51a7ab24c6c382cff4149
Pascal Böhler <pascal@bembel.dev>
2023-07-06 12:37:01 +0200

    feat: Changed design to adopt better to Corp Design of App

--- FaehrtSie-iOS/Views/CurrentJourneyTileView.swift
+++ FaehrtSie-iOS/Views/CurrentJourneyTileView.swift
@@ -14,79 +14,83 @@ struct CurrentJourneyTileView: View {
     var body: some View {
         VStack {
             Text("Your next trip")
-                .underline()
-                .font(.system(size: 30))
-            
-            HStack {
-                
-                Spacer()
-                
-                Image(systemName: "\(journey.mot.type.rawValue.lowercased())")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 100)
-                
-                Spacer()
-                
-                Image(journey.mot.iconName)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 75)
-                
-                Spacer()
-                
-            }
-                .padding(EdgeInsets(top: 5, leading: 15, bottom: 10, trailing: 15))
-            
-            HStack {
-                Text("Monday, \(journey.getDepartureDate())")
-                    .font(.system(size: 22))
-                Spacer()
-                Image(systemName: "clock").font(.system(size: 22))
-                Text("\(journey.getDepartureTime())")
-                    .font(.system(size: 22))
-            }
-                .padding(EdgeInsets(top: 10, leading: 15, bottom: 5, trailing: 15))
-            HStack {
-                Text("Status: ")
-                Text("\(!journey.isDelayed ? "On Time" : "Delayed \n\(journey.delay) Minutes" )")
-                    .foregroundColor(journey.isDelayed ? .red : Design.greenColor) // TODO: Find a better green! Changed to ForestGreen
-                Spacer()
-                
-            }
-                .font(.system(size: 22))
-                .padding(EdgeInsets(top: 5, leading: 15, bottom: 10, trailing: 15))
+                .font(.system(size: 40))
+                .padding(EdgeInsets(top: 10, leading: 5, bottom: -2, trailing: 5))
             
             VStack {
                 HStack {
-                    Image(systemName: "mappin.circle.fill")
-                    Text("\(journey.departureStation) at \(journey.getDepartureTime())")
+                    
                     Spacer()
-                }
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
-                HStack {
-                    Image(systemName: "ellipsis")
+                    
+                    Image(systemName: "\(journey.mot.type.rawValue.lowercased())")
+                        .resizable()
                         .scaledToFit()
-                        .frame(width: 22)
-                    Image(systemName: "clock")
-                    Text("20 Minutes")
-                        .font(.system(size: 17))
+                        .frame(height: 100)
+                    
                     Spacer()
+                    
+                    Image(journey.mot.iconName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 75)
+                    
+                    Spacer()
+                    
                 }
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
+                .padding(EdgeInsets(top: 5, leading: 15, bottom: 10, trailing: 15))
+                
                 HStack {
-                    Image(systemName: "flag.checkered.circle.fill")
-                    Text("\(journey.arrivalStation) at \(journey.getArrivalTime())")
+                    Text("Monday, \(journey.getDepartureDate())")
                     Spacer()
+                    Image(systemName: "clock")
+                    Text("\(journey.getDepartureTime())")
                 }
-            }
+                .font(.system(size: 22, weight: .semibold))
+                .padding(EdgeInsets(top: 10, leading: 15, bottom: 5, trailing: 15))
+                HStack {
+                    Text("Status: ")
+                    Text("\(!journey.isDelayed ? "On Time" : "Delayed \n\(journey.delay) Minutes" )")
+                        .foregroundColor(journey.isDelayed ? .red : Design.greenColor) // TODO: Find a better green! Changed to ForestGreen
+                    Spacer()
+                    
+                }
+                .font(.system(size: 22, weight: .semibold))
+                .padding(EdgeInsets(top: 5, leading: 15, bottom: 10, trailing: 15))
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "mappin.circle.fill")
+                        Text("\(journey.departureStation) at \(journey.getDepartureTime())")
+                        Spacer()
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
+                    HStack {
+                        Image(systemName: "ellipsis")
+                            .scaledToFit()
+                            .frame(width: 22)
+                        Image(systemName: "clock")
+                        Text("20 Minutes")
+                            .font(.system(size: 17))
+                        Spacer()
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
+                    HStack {
+                        Image(systemName: "flag.checkered.circle.fill")
+                        Text("\(journey.arrivalStation) at \(journey.getArrivalTime())")
+                        Spacer()
+                    }
+                }
                 .font(.system(size: 20))
                 .padding(EdgeInsets(top: 5, leading: 15, bottom: 10, trailing: 15))
+                
+            }
             
-        }
-            .foregroundColor(.black)
-            .padding(EdgeInsets(top: 15, leading: 5, bottom: 15, trailing: 5))
+            .padding(EdgeInsets(top: 20, leading: 5, bottom: 15, trailing: 5))
             .background(Design.darkBGColor)
+            .cornerRadius(25.0)
+        }
+            .background(Design.stdCorpColor)
+            .foregroundColor(.black)
             .cornerRadius(25.0)
             .padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 10))
     }
